ab16bd71284e89dd330d5e9410f01e6e14bb0f1e
lace04 <luisace04@gmail.com>
2023-09-24 14:56:25 -0500

    Admin Protection

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,24 +1,27 @@
-import { auth } from "@clerk/nextjs";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
+import { auth } from '@clerk/nextjs';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { isTeacher } from '@/lib/teacher';
+
 const f = createUploadthing();
- 
+
 const handleAuth = () => {
   const { userId } = auth();
-  if (!userId) throw new Error("Unauthorized");
+  const isAuthorized = isTeacher(userId);
+
+  if (!userId || !isAuthorized) throw new Error('Unauthorized');
   return { userId };
-}
+};
 
 export const ourFileRouter = {
-  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+  courseImage: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+  courseAttachment: f(['text', 'image', 'video', 'audio', 'pdf'])
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
-  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: '512GB' } })
     .middleware(() => handleAuth())
-    .onUploadComplete(() => {})
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,45 +1,47 @@
-"use client";
+'use client';
 
-import { UserButton } from "@clerk/nextjs";
-import { usePathname } from "next/navigation";
-import { LogOut } from "lucide-react";
-import Link from "next/link";
+import { UserButton, useAuth } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
+import { LogOut } from 'lucide-react';
+import Link from 'next/link';
 
-import { Button } from "@/components/ui/button";
-import { SearchInput } from "./search-input";
+import { Button } from '@/components/ui/button';
+import { isTeacher } from '@/lib/teacher';
+
+import { SearchInput } from './search-input';
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth();
   const pathname = usePathname();
 
-  const isTeacherPage = pathname?.startsWith("/teacher");
-  const isCoursePage = pathname?.includes("/courses");
-  const isSearchPage = pathname === "/search";
+  const isTeacherPage = pathname?.startsWith('/teacher');
+  const isCoursePage = pathname?.includes('/courses');
+  const isSearchPage = pathname === '/search';
 
   return (
     <>
-    {isSearchPage && (
-      <div className="hidden md:block">
-        <SearchInput />
-      </div>
-    )}
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isCoursePage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Salir
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Modo Profesor
-          </Button>
-        </Link>
+      {isSearchPage && (
+        <div className='hidden md:block'>
+          <SearchInput />
+        </div>
       )}
-      <UserButton
-        afterSignOutUrl="/"
-      />
-    </div></>
-  )
-}
+      <div className='flex gap-x-2 ml-auto'>
+        {isTeacherPage || isCoursePage ? (
+          <Link href='/'>
+            <Button size='sm' variant='ghost'>
+              <LogOut className='h-4 w-4 mr-2' />
+              Salir
+            </Button>
+          </Link>
+        ) : isTeacher(userId) ? (
+          <Link href='/teacher/courses'>
+            <Button size='sm' variant='ghost'>
+              Modo Profesor
+            </Button>
+          </Link>
+        ): null}
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </>
+  );
+};
