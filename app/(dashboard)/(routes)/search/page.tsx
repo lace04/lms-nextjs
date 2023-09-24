587510b8917ca97ee0a551eea97180ed50db90d2
lace04 <luisace04@gmail.com>
2023-09-23 20:54:46 -0500

    Course Grid and Card UI

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,53 @@
-import { db } from '@/lib/db';
-import Categories from './_components/categories';
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const SearchPage = async () => {
-  const categories = await db.category.findMany({
-    orderBy: {
-      name: 'asc',
-    },
-  });
-  return (
-    <div className='p-6'>
-      <Categories items={categories}/>
-      {/* {categories.map((category) => {
-        return <div key={category.id}>{category.name}</div>;
-      })}  */}
-    </div>
-  );
+import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
+
+import { Categories } from "./_components/categories";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  }
 };
 
+const SearchPage = async ({
+  searchParams
+}: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc"
+    }
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories
+          items={categories}
+        />
+        <CoursesList items={courses} />
+      </div>
+    </>
+   );
+}
+ 
 export default SearchPage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -6,14 +6,22 @@ import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
+    <>
+    {isSearchPage && (
+      <div className="hidden md:block">
+        <SearchInput />
+      </div>
+    )}
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
         <Link href="/">
@@ -32,6 +40,6 @@ export const NavbarRoutes = () => {
       <UserButton
         afterSignOutUrl="/"
       />
-    </div>
+    </div></>
   )
 }
